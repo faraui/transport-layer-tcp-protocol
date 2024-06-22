af1219cc840d74242489441dbff6e4d9add3fa44
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-22 14:27:04 +0200

    ++++ resend.pl

--- resend.pl
+++ resend.pl
@@ -3,35 +3,30 @@
 use warnings;
 use strict;
 use MIME::Base64;
-
 use FindBin;
 use lib $FindBin::Bin;
-use rottweiler;
+use transport-layer-tcp-protocol ;
 
 my $encoded_file = shift @ARGV;
 my $confirmed_file = shift @ARGV;
-
 open my $confirmed_handler,$confirmed_file or die $!;
 open my $encoded_handler,$encoded_file or die $!;
-
 my $confirmed_blocks = {};
 
 while (<$confirmed_handler>) {
-    s#\n##;
-    $confirmed_blocks->{$_} = 1;
+  s#\n##;
+  $confirmed_blocks->{$_} = 1;
 }
 close $confirmed_handler;
 
 print add_overhead();
-
 while(<$encoded_handler>) {
-    m#.{22}\d{10}(\d{10})(.*)\n#;
-    my $packet_number = decode_base64(substr($2, 0, $1 + 0));
-    if (!exists $confirmed_blocks->{$packet_number}) {
-        print $_;
-    }
+  m#.{22}\d{10}(\d{10})(.*)\n#;
+  my $packet_number = decode_base64(substr($2, 0, $1 + 0));
+  if (!exists $confirmed_blocks->{$packet_number}) {
+    print $_;
+  }
 }
 
 print add_overhead();
-
 close $encoded_handler;
